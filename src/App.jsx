23fbf4bb798b8bffbe3a982472486c7e9574ea95
istiak514 <istiakkhan514@gmail.com>
2026-05-06 16:14:36 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const VENUE = {
 
 const MEAL_OPTIONS = [
   { value: "Salmon", label: "🐟 Salmon" },
-  { value: "Steak", label: "🥩 Steak" },
+  { value: "Steak", label: "🥩 Steak (Halal)" },
   { value: "Vegetarian", label: "🥗 Vegetarian" },
 ];
 
